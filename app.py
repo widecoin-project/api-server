3d39192f929e55bdbbd7043694e8060bb946fe87
volbil <volbilnexus@gmail.com>
2020-03-03 17:55:58 +0900

    Roll back old initialization

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from server import config
-from server import sio
 from server import app
 
 if __name__ == "__main__":
-    sio.run(app, debug=config.debug, host=config.host, port=config.port)
+    app.run(debug=config.debug, host=config.host, port=config.port)
