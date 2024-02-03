cb9e6a3480f227a3126ba2ff7034268f04a619c0
DavidJack01 <widecoin.official@gmail.com>
2024-02-03 22:44:17 +0000

    fixed supply

--- server/utils.py
+++ server/utils.py
@@ -108,10 +108,10 @@ def supply(height):
         supply += total
 
     supply = supply + height * reward"""
-    print(sub_total_supply2)
+    data2 = make_request("gettxoutsetinfo")
     return {
         "halvings": int(1),
-        "supply": satoshis(sub_total_supply3),
+        "supply": satoshis(int(data2["result"]["total_amount"])),
         "total/max supply": satoshis(35000000)
         #"supply": type(str(sub_total_supply) + "00000000")
     }
